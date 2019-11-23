eb0592d268222d9d4ee78b76f1f92f6b776754cf
icreese <xycarto@gmail.com>
2019-11-23 20:03:03 +1300

    fix slashes

--- scripts/docker/tilemill_ubuntu/tilemill_run.sh
+++ scripts/docker/tilemill_ubuntu/tilemill_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker run /
-    -p 20009:20009 /
-    -p 20008:20008 /
-    -p 5482:5482 /
-    -v ~/testing:/home/icreese/testing /
-    -v ~/Documents/MapBox:/root/Documents/MapBox /
+sudo docker run \
+    -p 20009:20009 \
+    -p 20008:20008 \
+    -p 5482:5482 \
+    -v ~/testing:/home/icreese/testing \
+    -v ~/Documents/MapBox:/root/Documents/MapBox \
     -it tilemill:ubuntu18.04
 
