6d23eadc87bba9918b3708d09472e30614fbf4cc
icreese <xycarto@gmail.com>
2019-11-24 21:49:05 +1300

    update tilemill run

--- scripts/docker/tilemill_ubuntu/tilemill_run.sh
+++ scripts/docker/tilemill_ubuntu/tilemill_run.sh
@@ -4,7 +4,7 @@ sudo docker run \
     -p 20009:20009 \
     -p 20008:20008 \
     -p 5482:5482 \
-    -v ~/testing:/home/icreese/testing \
-    -v ~/Documents/MapBox:/root/Documents/MapBox \
+    -v ${HOME}:/home/${USER} \
+    -v ${HOME}/Documents/MapBox:/root/Documents/MapBox \
     -it tilemill:ubuntu18.04
 
