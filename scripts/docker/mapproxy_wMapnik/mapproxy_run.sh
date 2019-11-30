c5b4041a55c3d2cb3a62ba656081ad70887bdd14
icreese <xycarto@gmail.com>
2019-11-30 19:02:55 +1300

    edit run

--- scripts/docker/mapproxy_wMapnik/mapproxy_run.sh
+++ scripts/docker/mapproxy_wMapnik/mapproxy_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run  -p 8080:8080 -v ${PWD}:${PWD} -u $(id -u):$(id -g) -it mapproxy_python_mapnik:ubuntu16.04
+docker run  -p 8080:8080 -v ${PWD}:${PWD} -it mapproxy_python_mapnik:ubuntu16.04
 
 
