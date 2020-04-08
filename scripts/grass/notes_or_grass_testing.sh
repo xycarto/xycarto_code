1ee8d705b90c7ea8ba6eb3ed7474d867f3247192
Ireese <ireese@linz.govt.nz>
2020-04-09 10:27:55 +1200

    grass notes

--- scripts/grass/notes_or_grass_testing.sh
+++ scripts/grass/notes_or_grass_testing.sh
@@ -1,9 +1,14 @@
-rasters=/home/ireese/testing/hydrotesting/raster/BJ.tif 
+#testing for using saga gis to burn vectors into an elevation 
+#raster and create a rivers network.  The goal is to make rivers
+#follow pre-existing rivers drawn from aerial imagery.  Testing 
+#is first completed using LINZ 8m DEM and LINZ hydro lines 
 
-r.in.gdal --overwrite input=/home/ireese/testing/hydrotesting/raster/BJ.tif output=bj
+rasters=~/testing/hydrotesting/raster/BJ.tif 
+
+r.in.gdal --overwrite input=~/testing/hydrotesting/raster/BJ.tif output=bj
 
 r.info map=bj
 
 g.region rast=bj -p
 
-r.out.gdal input=bj output=/home/ireese/testing/hydrotesting/bj_test_GRASS/grasscollect/test.tif  nodata=-9999
+r.out.gdal input=bj output=~/testing/hydrotesting/bj_test_GRASS/grasscollect/test.tif  nodata=-9999
