0de5cdc9abe3d2817563109e54dee488780f6413
Ireese <ireese@linz.govt.nz>
2020-04-11 17:44:53 +1200

    building full test case

--- scripts/grass/network_by_watershed.sh
+++ scripts/grass/network_by_watershed.sh
@@ -52,9 +52,48 @@ do
 
     #check output
     echo "check output"
-    outfile=${outFinal}/${fileName}_id_${i}_check.tif
-    r.out.gdal input=$outRiversRast output=$outfile format=GTiff type=Float32 --overwrite
+    #outfile=${outFinal}/${fileName}_id_${i}_check.tif
+    #r.out.gdal input=$outRiversRast output=$outfile format=GTiff type=Float32 --overwrite
 
-    
+    #fill DEM
+    echo "fill dem"
+    #r.fill.dir [-f] input=name output=name direction=name [areas=name] [format=string] [--overwrite] [--help] [--verbose] [--quiet] [--ui] 
+    #TODO: depression=depression map of real depressions
+    filldem=${fileName}_filldem
+    directionDEM=${fileName}_directiondem
+    r.fill.dir input=$outRiversRast output=$filldem direction=$directionDEM --overwrite
+
+    #carve in rivers
+    echo "LINZ rivers carve"
+    #r.carve [-n] raster=name vector=name output=name [points=name] [width=float] [depth=float] [--overwrite] [--help] [--verbose] [--quiet] [--ui] 
+    carve=${fileName}_carve
+    r.carve -n raster=$filldem vector=$outRiversVect output=$carve depth=2 width=10 --overwrite
+
+    #create hydro DEM
+    echo "create hydro corrected dem"
+    #r.hydrodem [-afc] input=name [depression=name] [memory=integer] output=name mod=integer size=integer [--overwrite] [--help] [--verbose] [--quiet] [--ui] 
+    hydrodem=${fileName}_hydrodem
+    r.hydrodem input=$carve memory=3000 output=$hydrodem mod=4 size=4 --overwrite
+
+    #create watersheds
+    #TODO: check input watershed threshold value
+    echo "create watersheds"
+    #r.watershed [-s4mab] elevation=name [depression=name] [flow=name] [disturbed_land=name] [blocking=name] [retention=name] [threshold=integer] [max_slope_length=float] [accumulation=name] [tci=name] [spi=name] [drainage=name] [basin=name] [stream=name] [half_basin=name] [length_slope=name] [slope_steepness=name] [convergence=integer] [memory=integer] [--overwrite] [--help] [--verbose] [--quiet] [--ui] 
+    accumulation=${fileName}_accumulation
+    drainage=${fileName}_drainage
+    stream=${fileName}_stream
+    r.watershed elevation=$hydrodem threshold=20 accumulation=$accumulation drainage=$drainage stream=$stream memory=3000 --overwrite
+
+    #stream order
+    echo "stream order"
+    #r.stream.order [-zma] stream_rast=name direction=name [elevation=name] [accumulation=name] [stream_vect=name] [strahler=name] [horton=name] [shreve=name] [hack=name] [topo=name] [memory=integer] [--overwrite] [--help] [--verbose] [--quiet] [--ui] 
+    stream_vect=${fileName}_stream_vect
+    strahler=${fileName}_strahler
+    r.stream.order stream_rast=$stream direction=$drainage elevation=$hydrodem accumulation=$accumulation stream_vect=$stream_vect strahler=$strahler memory=3000 --overwrite
+
+    #export stream vector
+    echo "export stream order"
+    streamOut=${outFinal}/${fileName}_stream_vector_iii.gpkg
+    v.out.ogr input=$stream_vect output=$streamOut type=line format=GPKG --overwrite
 done
 
